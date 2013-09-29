8ac476a8f1e232969b0e5c86f6e19f0662472512
mjs0026 <mjs0026@tigermail.auburn.edu>
2013-09-29 16:18:41 -0500

    my udp client

---
+++ udpclient.c
@@ -0,0 +1,80 @@
+/*
+ ** talker.c -- a datagram "client" demo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVERPORT "10024"      // the port users will be connecting to
+
+
+typedef struct message{
+        short length;
+        char checksum;
+        char gid;
+        char requestID;
+        char *hostnames;
+} message;
+
+
+
+
+int main(int argc, char *argv[])
+{
+        int sockfd;
+        struct addrinfo hints, *servinfo, *p;
+        int rv;
+        int numbytes;
+
+        printf("%s, %s, %s, %s, %s ", argv[0], argv[1], argv[2], argv[3], argv[4]);
+        if (argc != 4) {
+                fprintf(stderr,"usage: client server port request \"hostnames\"\n");
+                exit(1);
+        }
+
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+hints.ai_socktype = SOCK_DGRAM;
+
+        if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+                return 1;
+        }
+
+        // loop through all the results and make a socket
+        for(p = servinfo; p != NULL; p = p->ai_next) {
+                if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1) {
+                        perror("talker: socket");
+                        continue;
+                }
+
+                break;
+        }
+
+        if (p == NULL) {
+                fprintf(stderr, "talker: failed to bind socket\n");
+                return 2;
+        }
+
+        if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+                           p->ai_addr, p->ai_addrlen)) == -1) {
+                perror("talker: sendto");
+                exit(1);
+        }
+
+        freeaddrinfo(servinfo);
+
+        printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+        close(sockfd);
+
+        return 0;
+}
